c7e97f10ccef63bb0103d6301ee92759de65cf2a
Marco Postma <M.A.Postma@student.tudelft.nl>
2017-12-19 12:00:04 +0100

    vga_controller less registers

--- plot.tcl
+++ plot.tcl
@@ -1,5 +1,7 @@
-read_file -f ddc ./ADB/helicopter.ddc
-set designer vschonk
+set link_library [list "*" "./ADB/divider.ddc" "./ADB/draw.ddc" "./ADB/helicopter.ddc" "./ADB/pos.ddc" "./ADB/posnew.ddc" "./ADB/vga_tb.ddc" "/data/public/common/software/opprog/synth_libs/g_digilib5_99.db" "/data/public/common/software/opprog/synth_libs/g_analib8_00.db" "/data/public/common/software/opprog/synth_libs/buffers.db"]
+
+read_file -f ddc ./ADB/vga_controller.ddc
+set designer mapostma
 set company TU-Delft
 start_gui
 gui_create_schematic
