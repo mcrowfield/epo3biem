8a909db2f76f6b182fa4c714623c1ac5d03a0803
michaelkraaije <michaelkraaije@tud208898.ws.tudelft.net>
2017-12-19 10:07:15 +0100

    start posnew layout

--- synthesize.tcl
+++ synthesize.tcl
@@ -1,5 +1,5 @@
 #*********************************************************
-#  synthesize script for cell: pos                        *
+#  synthesize script for cell: posnew                     *
 #*********************************************************
 set hdlin_vhdl_93 false
 set hdlin_ff_always_async_set_reset true
@@ -7,12 +7,12 @@ set hdlin_ff_always_sync_set_reset true
 set vhdlout_architecture_name "synthesised"
 set vhdlout_use_packages {"ieee.std_logic_1164" "CellsLib.CellsLib_DECL_PACK"}
 set company "ontwerp_practicum"
-set designer "vschonk"
+set designer "michaelkraaije"
 set target_library  {"/data/public/common/software/opprog/synth_libs/g_digilib5_99.db"}
-set link_library [list "*" "/data/public/common/software/opprog/synth_libs/g_digilib5_99.db" "/data/public/common/software/opprog/synth_libs/g_analib8_00.db" "/data/public/common/software/opprog/synth_libs/buffers.db"]
+set link_library [list "*" "./ADB/divider.ddc" "./ADB/draw.ddc" "./ADB/helicopter.ddc" "./ADB/pos.ddc" "./ADB/vga_controller.ddc" "./ADB/vga_tb.ddc" "/data/public/common/software/opprog/synth_libs/g_digilib5_99.db" "/data/public/common/software/opprog/synth_libs/g_analib8_00.db" "/data/public/common/software/opprog/synth_libs/buffers.db"]
 define_design_lib MY_LIB -path ./syn_work
 define_design_lib CELLSLIB -path /data/public/common/software/opprog/synth_libs/CellsLib
-read_file -format vhdl -work MY_LIB {VHDL/pos.vhd }
+read_file -format vhdl -work MY_LIB {VHDL/pos+.vhd }
 set_dont_touch {g_analib8_00/*}
 # set_max_fanout 1.8 all_inputs() 
 # set_max_area 1000 
@@ -21,8 +21,8 @@ compile
 ungroup -all -flat
 report_area
 report_fsm
-write_file -f ddc pos -output ./ADB/pos.ddc
-write_file -f vhdl pos -output ./VHDL/pos_SYNTH.vhd
+write_file -f ddc posnew -output ./ADB/posnew.ddc
+write_file -f vhdl posnew -output ./VHDL/posnew_SYNTH.vhd
 quit
 
 
