4b9b3041a7650d929460136256af0cec2414e044
michaelkraaije <michaelkraaije@tud208898.ws.tudelft.net>
2017-12-19 12:20:12 +0100

    help

--- plot.tcl
+++ plot.tcl
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 set link_library [list "*" "./ADB/divider.ddc" "./ADB/draw.ddc" "./ADB/helicopter.ddc" "./ADB/pos.ddc" "./ADB/posnew.ddc" "./ADB/vga_tb.ddc" "/data/public/common/software/opprog/synth_libs/g_digilib5_99.db" "/data/public/common/software/opprog/synth_libs/g_analib8_00.db" "/data/public/common/software/opprog/synth_libs/buffers.db"]
 
 read_file -f ddc ./ADB/vga_controller.ddc
@@ -8,6 +9,10 @@ set designer mapostma
 read_file -f ddc ./ADB/pos.ddc
 set designer jrkoning
 >>>>>>> 0e03a5bee655ae6f852fec6836b16c3ee6a3c259
+=======
+read_file -f ddc ./ADB/helicopter.ddc
+set designer vschonk
+>>>>>>> parent of c7e97f1... vga_controller less registers
 =======
 read_file -f ddc ./ADB/helicopter.ddc
 set designer vschonk
