3baeb7aa7a206ee8133c7ecaccbd2a6bac5f472a
VictorSchonk <victorschonk@hotmail.com>
2018-01-11 11:22:27 +0100

    Update collide.vhd

--- VHDL/collide.vhd
+++ VHDL/collide.vhd
@@ -1,1 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity collide is
+
+    port(
+      blue_in   : in std_logic;
+      red_in    : in std_logic;
+      blue_out  : out std_logic;
+      red_out   : out std_logic;
+      collision : out std_logic
+    );
+end collide;
